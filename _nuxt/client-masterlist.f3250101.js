9e452bc8b6dfde33f3b739c87f0b055b91c77bd4
DESKTOP-SH1NE0T\Raldin Casidar <raldincasidar@gmail.com>
2023-03-26 13:05:27 +0800

    first commit

---
+++ _nuxt/client-masterlist.f3250101.js
@@ -0,0 +1,13 @@
+import{r as e,a as t,o as l,b as o,e as s,w as b,u as p,F as c,f as d}from"./entry.4aa60264.js";const m=["innerHTML"],v={__name:"client-masterlist",setup(P){e(null);const n=e([{type:"subheader",title:"Client's List"},{prependAvatar:"https://cdn.vuetifyjs.com/images/lists/1.jpg",title:"Raldin C. Disomimba",subtitle:`
+            <span class="text-red text-bold">Total Balance: <b>PHP 13,212</b> </span> <br>
+            Investment: <b>PHP 2,000 </b> <br>
+            Bonus Earned: <b>PHP 420</b> <br>
+            Referal: <b>PHP 420</b> <br> 
+            Points: <b>PHP 420</b> 
+          `},{type:"divider",inset:!0},{prependAvatar:"https://cdn.vuetifyjs.com/images/lists/1.jpg",title:"Jebray Sorronda",subtitle:`
+            <span class="text-red text-bold">Total Balance: <b>PHP 13,212</b> </span> <br>
+            Investment: <b>PHP 2,000 </b> <br>
+            Bonus Earned: <b>PHP 420</b> <br>
+            Referal: <b>PHP 420</b> <br> 
+            Points: <b>PHP 420</b> 
+          `},{type:"divider",inset:!0}]);return(u,_)=>{const a=t("v-text-field"),r=t("v-list");return l(),o(c,null,[s(a,{outlined:"",label:"Search Users",class:"ma-5","append-inner-icon":"mdi-magnify"}),s(r,{items:p(n),"item-props":"",lines:"four",link:"",to:"/404",ripple:""},{subtitle:b(({subtitle:i})=>[d("div",{innerHTML:i},null,8,m)]),_:1},8,["items"])],64)}}};export{v as default};
---
+++ _nuxt/composables.2c6b83b4.js
@@ -0,0 +1,1 @@
+import{h as r}from"./entry.4aa60264.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
